e4c4e98de41541f13e32ad50b1996f460df577c6
GlenIsIsaw <glenpabico18@gmail.com>
2025-10-14 15:57:10 +0800

    Fixing Bugs on App

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import Home from "./pages/Home";
 import Project from "./pages/Project";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
 
 function App() {
   useEffect(() => {
